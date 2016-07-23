f20e9bf7adfc9b0e1abd7b0430e21532edf61468
Sam Farmer <sam_farmer@hotmail.com>
2016-07-23 16:39:13 +0100

    Send emails in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 
   config.active_support.deprecation = :log
 
