95d30fad95e4f8b9a56289dce6892d7ae5093d2e
Sam Farmer <sam_farmer@hotmail.com>
2016-07-23 16:44:27 +0100

    Delayed job for sendin devise emails
      - method added to user model to handle it

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name
 
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
 end
