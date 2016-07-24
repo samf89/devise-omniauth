42a94e86fcba24c2ad6861548594b7813d2d24d5
Sam Farmer <sam_farmer@hotmail.com>
2016-07-24 14:53:57 +0100

    Find user by auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
+         :confirmable, :lockable,
+         :omniauthable, omniauthable_provides: [:facebook]
 
   validates_presence_of :name
+
+  class << self
+    def from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+      end
+    end
+  end
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
