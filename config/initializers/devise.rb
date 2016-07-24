4b61b2b3ab7276ad3eb256151d5ec4de31561a9b
Sam Farmer <sam_farmer@hotmail.com>
2016-07-24 14:46:07 +0100

    Facebook omniauth added to devise
      - id and secret stored in file added to .gitignore

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,29 +1,21 @@
 Devise.setup do |config|
 
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'mailer@example.com'
 
   require 'devise/orm/active_record'
+  require_relative 'facebook_omniauth'
 
   config.case_insensitive_keys = [:email]
-
   config.strip_whitespace_keys = [:email]
-
   config.skip_session_storage = [:http_auth]
-
   config.stretches = Rails.env.test? ? 1 : 11
-
   config.reconfirmable = true
-
   config.expire_all_remember_me_on_sign_out = true
-
   config.password_length = 6..128
-
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
-
   config.reset_password_within = 6.hours
 
   config.sign_out_via = :delete
-
   config.lock_strategy = :failed_attempts
   config.unlock_strategy = :both
   config.maximum_attempts = 5
@@ -32,5 +24,8 @@ Devise.setup do |config|
 
   config.confirm_within = 3.days
   config.reconfirmable = true
+
+  # Omniauth for facebook 
+  config.omniauth :facebook, FACEBOOK_ID, FACEBOOK_SECRET
 
 end
