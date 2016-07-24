2e1c4cd9f55a2e00cc7564acfab11fb62af3e8bf
Sam Farmer <sam_farmer@hotmail.com>
2016-07-24 14:56:53 +0100

    Route for omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { 
+    registrations: 'registrations',
+    omniauth_callbacks: 'callbacks'
+  }
+
   resources :products
 
   root 'pages#index'
